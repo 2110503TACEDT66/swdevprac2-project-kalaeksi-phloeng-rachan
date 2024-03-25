9e472381d9d396044df00312d7dc322aa3ebbd0a
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-03-26 06:31:17 +0700

    add project color palette to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-darker": "#303841",
+        "gray-lighter": "#3A4750",
+        "gray-blueish": "#203541",
+        "yellow-sour": "#FFD933",
+        "pink-sweet": "#F62A66"
+      }
+    },
   },
   plugins: [],
 }
