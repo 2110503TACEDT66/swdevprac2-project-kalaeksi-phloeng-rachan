479db52e163b6d884892b4558a605988752fa336
Natthaphong Saneubon <6633075321@student.chula.ac.th>
2024-03-26 20:10:51 +0700

    register login

--- src/app/api/auth/login/page.tsx
+++ src/app/api/auth/login/page.tsx
@@ -16,40 +16,40 @@ export default function Login() {
 
   return (
     <main className="h-screen w-full flex items-center justify-center">
-      
-        <div className="md:w-[23%] w-full bg-black md:rounded-r-lg rounded-lg p-6 flex flex-col justify-center items-center" style={{ height: '600px' }}>
-          <h1 className={`text-4xl pb-10 font-extrabold text-center text-white`}>Login</h1>
-          <form className="w-full" action={handleSubmit}>
-            <div className="p-4 space-y-2">
-              <label htmlFor="email" className={`text-xl text-white`}> Email </label>
-              <input
-                type="email"
-                required
-                name="email"
-                placeholder="Email"
-                className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
-              />
-            </div>
-            <div className="p-4 space-y-2">
-              <label className={`text-xl text-white`}>Password</label>
-              <input
-                type="password"
-                required
-                name="password"
-                placeholder="Password"
-                className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
-              />
-            </div>
-            <div className="flex justify-center">
-            <Button
+      <div className="md:w-[23%] w-full bg-black md:rounded-r-lg rounded-lg p-6 flex flex-col justify-center items-center relative" style={{ height: '600px', backgroundColor: '#1E1E1E' }}>
+        <h1 className={`text-4xl pb-10 font-extrabold text-center text-white absolute top-10`}>Login</h1>
+        <form className="w-full" action={handleSubmit}>
+          <div className="p-4 space-y-2">
+            <label htmlFor="email" className={`text-xl text-white font-bold`}> Email </label>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Email"
+              className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
+            />
+          </div>
+          <div className="p-4 space-y-2">
+            <label className={`text-xl text-white font-bold`}>Password</label>
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Password"
+              className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
+            />
+          </div>
+          <div className="flex justify-center">
+          <Button
               type="submit"
-              className="w-[10%] font-serif text-black bg-yellow-500 hover:bg-yellow-600 my-2"
+              className="w-[40%] font-bold text-black bg-yellow-500 hover:bg-yellow-600 my-2"
+              style={{ backgroundColor: '#FFD933' }}
             >
               Login
             </Button>
-            </div>
-          </form>
-        </div>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
--- src/app/api/auth/register/page.tsx
+++ src/app/api/auth/register/page.tsx
@@ -5,7 +5,6 @@ import { register } from "@/services/user/user.service";
 import { IRegister } from "@/interfaces/user/login.interface";
 import { redirect } from "next/navigation";
 
-
 export default function Register() {
   const [passwordError, setPasswordError] = useState(false);
   const [passwordNotMatch, setPasswordNotMatch] = useState(false);
@@ -36,47 +35,44 @@ export default function Register() {
     };
 
     await register(registerForm);
-    redirect("/api/auth/login");
+    redirect("/");
   };
 
   return (
     <main className="h-full md:h-screen md:py-4 w-full flex items-center justify-center">
-  
-    <div className="md:w-[23%] w-full bg-black md:rounded-r-lg rounded-lg p-6 flex flex-col justify-center items-center">
-      <h1 className="text-4xl pb-10 font-extrabold text-center text-white">
-        Register
-        
-      </h1>
-      <form className="w-full" action={handleSubmit}>
-        <div className="p-4 space-y-2">
-          <label htmlFor="username" className="text-xl text-white">
-            Username
-          </label>
-          <input
-            type="text"
-            required
-            name="username"
-            placeholder="Username"
-            className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
-          />
-        </div>
-        <div className="p-4 space-y-2">
-          <label htmlFor="email" className="text-xl text-white">
-            Email
-          </label>
-          <input
-            type="email"
-            required
-            name="email"
-            placeholder="example@gmail.com"
-            className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
-          />
-        </div>
+      <div className="md:w-[23%] w-full bg-black md:rounded-r-lg rounded-lg p-6 flex flex-col justify-center items-center" style={{ backgroundColor: '#1E1E1E' }}>
+        <h1 className="text-4xl pb-10 font-extrabold text-center text-white">
+          Register
+        </h1>
+        <form className="w-full" action={handleSubmit}>
+          <div className="p-4 space-y-2">
+            <label htmlFor="username" className="text-xl text-white font-bold">
+              Username
+            </label>
+            <input
+              type="text"
+              required
+              name="username"
+              placeholder="Username"
+              className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
+            />
+          </div>
+          <div className="p-4 space-y-2">
+            <label htmlFor="email" className="text-xl text-white font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="example@gmail.com"
+              className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
+            />
+          </div>
 
-        <div className="flex md:flex-row flex-col">
-          <div className="p-4 space-y-2 md:w-3/4">
-            <label htmlFor="tel" className="text-xl text-white">
-              Telephone Number
+          <div className="p-4 space-y-2">
+            <label htmlFor="tel" className="text-xl text-white font-bold">
+              Phone
             </label>
             <input
               type="text"
@@ -86,33 +82,31 @@ export default function Register() {
               className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
             />
           </div>
-    
-        </div>
 
-        <div className="p-4 space-y-2">
-          <div className="flex flex-row space-x-4">
-            <label className="text-xl text-white">Password</label>
-            
+          <div className="p-4 space-y-2">
+            <div className="flex flex-row space-x-4">
+              <label className="text-xl text-white font-bold">Password</label>
+            </div>
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Password"
+              className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
+            />
           </div>
-          <input
-            type="password"
-            required
-            name="password"
-            placeholder="Password"
-            className="bg-white border-2 border-gray-200 rounded-lg w-full p-2 text-gray-700 focus:outline-none focus:border-primary"
-          />
-        </div>
-        
-        <div className="flex justify-center">
-          <Button
-            type="submit"
-            className="w-[40%] font-serif text-black bg-yellow-500 hover:bg-yellow-600 my-2"
-          >
-            Register
-          </Button>
-        </div>
-      </form>
-    </div>
-</main>
+          
+          <div className="flex justify-center">
+            <Button
+              type="submit"
+              className="w-[40%] font-bold text-black bg-yellow-500 hover:bg-yellow-600 my-2"
+              style={{ backgroundColor: '#FFD933' }}
+            >
+              Register
+            </Button>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
