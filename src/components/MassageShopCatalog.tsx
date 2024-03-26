a9bfa5b1aa982d6394776035da5d9885103d7c6b
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-03-26 20:40:33 +0700

    massageShop srcImage

--- src/components/MassageShopCatalog.tsx
+++ src/components/MassageShopCatalog.tsx
@@ -1,16 +1,17 @@
 import ProductCard from "./ProductCard"
 
-export default async function MassageShopCatalog({massageShopJson}: {massageShopJson: MassageShopJson}) {
-    const massageShopJsonReady = await massageShopJson;
+export default function MassageShopCatalog({massageShopJson}: {massageShopJson: MassageShopJson}) {
+    const massageShopJsonReady = massageShopJson;
     return (
         <div>
             <h3 className="text-md mt-[10px]">Explore {massageShopJsonReady.count} massage shops in our catalog</h3>
             <div className="flex flex-col items-center">
                 {
                     massageShopJsonReady.data.map((massageShopItem: MassageShopItem)=>(
+                        
                         <ProductCard massageShopName={massageShopItem.name}
                                     massageShopAddress={massageShopItem.address}
-                                    imgSrc="/img/massage1.jpg"
+                                    imgSrc={'/img/massage2.jpg'}
                                     massageShopRating={5}
                                     massageShopId={massageShopItem.id}/>
                     ))
