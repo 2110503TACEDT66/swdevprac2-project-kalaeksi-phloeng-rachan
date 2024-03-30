cbcb9bb6cc5de6be3c27ee9b593f7b5f9bc76e3a
LapisBerry <141068821+LapisBerry@users.noreply.github.com>
2024-03-30 22:13:24 +0700

    Banner test

--- __test__/Banner.test.tsx
+++ __test__/Banner.test.tsx
@@ -63,22 +63,3 @@ describe('Banner', ()=>{
         expect(rightImage).toHaveStyle({"width":450});
     })
 })
-
-/**
- * it('should have these element', ()=>{
-        render(<Banner/>);
-        const bannerContainer = screen.getByTestId("banner-container");
-        const leftImage = screen.getByTestId("left-image");
-        const leftArrow = screen.getByTestId("left-arrow");
-        const middleImage = screen.getByTestId("middle-image");
-        const rightArrow = screen.getByTestId("right-arrow");
-        const rightImage = screen.getByTestId("right-image");
-        
-        expect(bannerContainer).toBeInTheDocument();
-        expect(leftImage).toBeInTheDocument();
-        expect(leftArrow).toBeInTheDocument();
-        expect(middleImage).toBeInTheDocument();
-        expect(rightArrow).toBeInTheDocument();
-        expect(rightImage).toBeInTheDocument();
-    })
- */
